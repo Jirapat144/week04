e503b55c64084b80fcc9b9185786752b0b18af6c
Jirapat17412 <tangmo17412@hotmail.com>
2017-09-01 10:47:54 +0700

    ED by W4P05

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Lab01
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("000000000111111111112");
+            Console.WriteLine("123456789011234567890");
+            Console.WriteLine("{0, 0}", 1);
+            Console.WriteLine("{0, 1}", 1);
+            Console.WriteLine("{0, 2}", 1);
+            Console.WriteLine("{0, 3}", 1);
+            Console.WriteLine("{0, 5}", 1);
+            Console.WriteLine("{0, 10}", 1);
+            Console.WriteLine("{0, 15}", 1);
+            Console.WriteLine("{0, 20}", 1);
         }
     }
 }
