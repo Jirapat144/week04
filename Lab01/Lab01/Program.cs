7df7d0a4ef54c3832a21b07fda777f9b5b939c29
Jirapat17412 <tangmo17412@hotmail.com>
2017-09-07 14:59:06 +0700

    ED by W4P16

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -1,15 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Lab01
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int a, b, c, d, e, f;
+            a = 1;
+            b = a + 6;
+            c = b - 3;
+            d = c * 2;
+            e = d / 2;
+            f = e % 2;
         }
     }
 }
