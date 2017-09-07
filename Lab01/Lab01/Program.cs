1d92d4ca7858fa57e8a0e1f6cdbb5253e3fc11ac
Jirapat17412 <tangmo17412@hotmail.com>
2017-09-07 16:56:29 +0700

    ED by W4P14

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab01
 {
@@ -10,6 +6,9 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 100);
+            Console.WriteLine(randomNumber);
         }
     }
 }
