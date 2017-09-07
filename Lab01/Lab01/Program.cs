93abae0e3fd50cc86d35e5472cc8bb5200a71c23
Jirapat17412 <tangmo17412@hotmail.com>
2017-09-07 12:15:00 +0700

    ED by W4P12

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab01
 {
@@ -10,6 +6,19 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.Write("Please enter value 1 :");
+                int val1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Please enter value 2 :");
+                int val2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Answer = " + (val1 + val2));
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error : " + e.ToString());
+
+            }
         }
     }
 }
