74dbe9c54cdb19c48cce42395e8a9ed6db01a91b
Jirapat17412 <tangmo17412@hotmail.com>
2017-09-07 16:12:37 +0700

    ED by Imformation

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -10,6 +10,22 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+            string name, lastname;
+            int id;
+            double gpa;
+            Console.Write("Enter your name : ");
+            name = Console.ReadLine();
+            Console.Write("Enter your lastname : ");
+            lastname = Console.ReadLine();
+            Console.Write("Enter your id student : ");
+            id = int.Parse(Console.ReadLine());
+            Console.Write("Enter your GPA : ");
+            gpa = double.Parse(Console.ReadLine());
+            Console.WriteLine("Your Data");
+            Console.WriteLine("Name : {0} {1}", name, lastname);
+            Console.WriteLine("ID   : {0}", id);
+            Console.WriteLine("GPA  : {0}", gpa);
+            Console.ReadKey();
         }
     }
 }
